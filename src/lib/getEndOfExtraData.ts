3b6a46dd1393b27fea7e9317e1ef5f86843e599b
Tyler <tylergross28@gmail.com>
2023-08-12 14:19:16 -0400

    prevent byte slicing

--- src/lib/getEndOfExtraData.ts
+++ src/lib/getEndOfExtraData.ts
@@ -1,5 +1,11 @@
 import { ARCHIVE_SIGNATURE_LENGTH } from './arrchiveSignatureLength';
 
 export function getEndOfExtraData(bytes: Uint8Array) {
-	return bytes.slice(ARCHIVE_SIGNATURE_LENGTH).findIndex((byte) => byte === 0);
+	for (let i = ARCHIVE_SIGNATURE_LENGTH; i < bytes.length; i++) {
+		if (bytes[i] === 0) {
+			return i;
+		}
+	}
+
+	return 0;
 }
