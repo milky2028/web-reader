7fa97f8a16283f6fa08a5d160f837f26e9aa0126
Tyler <tylergross28@gmail.com>
2023-08-12 14:48:40 -0400

    fix os data

--- src/lib/getOperatingSystem.ts
+++ src/lib/getOperatingSystem.ts
@@ -1,11 +1,10 @@
 export function getOperatingSystem(uint32: number) {
 	switch (uint32) {
 		case 0:
-		case 10:
+		case 11:
 			return 'Windows';
 		case 3:
 		case 7:
-		case 19:
 			return 'MacOS';
 
 		default:
