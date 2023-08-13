10d7005b5b3f97e9234723ee9df3141447c846f6
Tyler <tylergross28@gmail.com>
2023-08-13 09:40:26 -0400

    listen for key

--- src/routes/book/[bookName]/page/[pageName]/+page.svelte
+++ src/routes/book/[bookName]/page/[pageName]/+page.svelte
@@ -4,6 +4,16 @@
 
 	let url: string;
 
+	function listenForArrowKey(event: KeyboardEvent) {
+		if (event.key === 'ArrowRight') {
+			console.log('go right');
+		}
+
+		if (event.key === 'ArrowLeft') {
+			console.log('go left');
+		}
+	}
+
 	onMount(async () => {
 		const { books } = await import('$lib/directories');
 		const bookHandle = await books.getDirectoryHandle($page.params.bookName);
@@ -20,3 +30,4 @@
 
 <div><a href="/books">Books</a></div>
 <img src={url} loading="lazy" alt="" width="700" />
+<svelte:window on:keydown={listenForArrowKey} />
