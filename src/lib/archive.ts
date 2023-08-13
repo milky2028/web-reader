605225ca974b5ca8374f98db06612e7d333f8368
Tyler <tylergross28@gmail.com>
2023-08-12 20:17:21 -0400

    fix compressed file location

--- src/lib/archive.ts
+++ src/lib/archive.ts
@@ -1,6 +1,8 @@
 import { Archive as libarchive } from 'libarchive.js';
 import workerUrl from '$lib/assets/worker-bundle.js?url';
+import { CompressedFile as libarchiveCompressedFile } from 'libarchive.js/src/compressed-file';
 
 libarchive.init({ workerUrl });
 
+export const CompressedFile = libarchiveCompressedFile;
 export const Archive = libarchive;
