e4d39591c063c0a2e91c63d6916034f4cc6fc324
Tyler <tylergross28@gmail.com>
2023-08-12 20:14:16 -0400

    paralell processing for uploads

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { CompressedFile } from 'libarchive.js/src/compressed-file';
 
 	async function onChange(event: Event & { currentTarget: HTMLInputElement }) {
 		const { Archive } = await import('$lib/archive');
 		const { cache } = await import('$lib/cache');
 
 		const files = (event.target as HTMLInputElement)?.files ?? [];
-		for (const file of files) {
+		const processFiles = Array.from(files).map(async (file) => {
 			const archive = await Archive.open(file);
-			const archivedFiles = await archive.getFilesObject();
+			const archivedFiles = await archive.getFilesArray();
+			const imageFiles = archivedFiles.filter(
+				({ file }) => file.name.endsWith('.jpg') || file.name.endsWith('.jpeg')
+			);
 
-			const frontCover = await archive.extractSingleFile(Object.keys(archivedFiles)[0]);
-			const cacheKey = `${file.name}+${frontCover.name}`;
+			const coverRef = imageFiles[0].file;
+			const cacheKey = `${file.name}+${coverRef.name}`;
+			const cacheMatch = await cache.match(cacheKey);
 
-			await cache.put(cacheKey, new Response(frontCover, { status: 200 }));
-			goto('/books');
-		}
+			if (!cacheMatch && coverRef instanceof CompressedFile) {
+				const cover = await coverRef.extract();
+				await cache.put(cacheKey, new Response(cover, { status: 200 }));
+			}
+		});
+
+		await Promise.all(processFiles);
+		goto('/books');
 	}
 </script>
 
