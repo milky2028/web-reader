4735aeaa6676193221a41508a962b9b01489aeed
Tyler <tylergross28@gmail.com>
2023-08-14 09:19:29 -0400

    don't try to cache past last page

--- src/lib/cachePages.ts
+++ src/lib/cachePages.ts
@@ -2,7 +2,7 @@ import { getPage } from './getPage';
 import { range } from './range';
 import { writeFile } from './writeFile';
 
-const NUMBER_OF_PAGES_TO_CACHE = 3;
+const NUMBER_OF_PAGES_TO_CACHE = 6;
 export async function cachePages(start: number, book: string) {
 	const { books } = await import('$lib/directories');
 	const { getManifest } = await import('$lib/manifest');
@@ -10,10 +10,12 @@ export async function cachePages(start: number, book: string) {
 	const bookHandle = await books.getDirectoryHandle(book);
 	const { pages } = await getManifest(book);
 
-	const process = range({ start, length: NUMBER_OF_PAGES_TO_CACHE }).map(async (index) => {
-		const page = await getPage(index, book);
-		return writeFile(pages[index], bookHandle, page);
-	});
+	const process = range({ start, length: NUMBER_OF_PAGES_TO_CACHE, max: pages.length }).map(
+		async (index) => {
+			const page = await getPage(index, book);
+			return writeFile(pages[index], bookHandle, page);
+		}
+	);
 
 	return Promise.all(process);
 }
--- src/lib/range.ts
+++ src/lib/range.ts
@@ -1,8 +1,9 @@
-type RangeParams = { start?: number; length: number; step?: number };
+type RangeParams = { start?: number; length: number; step?: number; max?: number };
 
-export function range({ start = 0, length }: RangeParams) {
+export function range({ start = 0, length, step = 1, max = start + length }: RangeParams) {
 	const numbers = [];
-	for (let i = start; i < start + length; i++) {
+
+	for (let i = start; i < start + length && i < max; i += step) {
 		numbers.push(i);
 	}
 
