38ab5798649d0ade41eb1676ad41206b3f72165a
Tyler <tylergross28@gmail.com>
2023-08-15 13:00:30 -0400

    remove unnessary shit

--- src/lib/cachePages.ts
+++ src/lib/cachePages.ts
@@ -1,21 +1,14 @@
 import { getPage } from './getPage';
 import { range } from './range';
-import { writeFile } from './writeFile';
 
 const NUMBER_OF_PAGES_TO_CACHE = 6;
 export async function cachePages(start: number, book: string) {
-	const { books } = await import('$lib/directories');
 	const { getManifest } = await import('$lib/manifest');
-
-	const bookHandle = await books.getDirectoryHandle(book);
 	const { pages } = await getManifest(book);
 
-	const process = range({ start, length: NUMBER_OF_PAGES_TO_CACHE, max: pages.length }).map(
-		async (index) => {
-			const page = await getPage(index, book);
-			return writeFile(pages[index], bookHandle, page);
-		}
+	const processPages = range({ start, length: NUMBER_OF_PAGES_TO_CACHE, max: pages.length }).map(
+		(index) => getPage(index, book)
 	);
 
-	return Promise.all(process);
+	return Promise.all(processPages);
 }
