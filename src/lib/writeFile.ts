9d5ad3d577fb224163e7038abe31514b29a9055d
Tyler <tylergross28@gmail.com>
2023-08-17 15:57:17 -0400

    allow writing safari

--- src/lib/writeFile.ts
+++ src/lib/writeFile.ts
@@ -1,5 +1,9 @@
 export async function writeFile(name: string, directory: FileSystemDirectoryHandle, content: File) {
 	const fileHandle = await directory.getFileHandle(name, { create: true });
-	const writeStream = await fileHandle.createWritable();
-	return content.stream().pipeTo(writeStream);
+	if ('createWritable' in fileHandle) {
+		const writeStream = await fileHandle.createWritable();
+		return content.stream().pipeTo(writeStream);
+	}
+
+	return Promise.resolve();
 }
