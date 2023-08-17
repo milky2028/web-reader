05f74db89264aa593e7a3a4ef7aeae6f834745cd
Tyler <tylergross28@gmail.com>
2023-08-17 11:39:44 -0400

    basics for showing two pages

---
+++ src/lib/isLandscapeMode.ts
@@ -0,0 +1,9 @@
+import { readable } from 'svelte/store';
+
+export const isLandscapeMode = readable(globalThis.innerWidth > globalThis.innerHeight, (set) => {
+	globalThis.addEventListener?.('resize', () =>
+		set(globalThis.innerWidth > globalThis.innerHeight)
+	);
+});
+
+isLandscapeMode.subscribe(($isLandscape) => console.log($isLandscape));
--- src/routes/book/[bookName]/page/[pageNumber]/+page.svelte
+++ src/routes/book/[bookName]/page/[pageNumber]/+page.svelte
@@ -2,36 +2,59 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { books, getPage } from '$lib/bookStore';
+	import { isLandscapeMode } from '$lib/isLandscapeMode';
 	import { readable } from 'svelte/store';
 
-	let pageUrl = readable('');
-	$: pageUrl = getPage(+$page.params.pageNumber, $page.params.bookName);
+	let firstPageUrl = readable('');
+	$: firstPageUrl = getPage(+$page.params.pageNumber, $page.params.bookName);
+
+	let secondPageUrl = readable('');
+	$: secondPageUrl = getPage(+$page.params.pageNumber + 1, $page.params.bookName);
 
 	async function onArrow(event: KeyboardEvent) {
+		const numberOfPagesToIncrement = showingTwoPages ? 2 : 1;
+
 		const pages = $books.get($page.params.bookName)?.pages ?? [];
 		const currentIndex = +$page.params.pageNumber;
 
 		if (event.key === 'ArrowRight') {
 			const lastPage = pages.length - 1;
-			const nextPage = currentIndex + 1 >= lastPage ? lastPage : currentIndex + 1;
 
-			await books.createPage(nextPage, $page.params.bookName, $books);
-			books.createPage(nextPage + 1, $page.params.bookName, $books);
+			const nextPage =
+				currentIndex + numberOfPagesToIncrement >= lastPage
+					? lastPage
+					: currentIndex + numberOfPagesToIncrement;
+
+			await Promise.all([
+				books.createPage(nextPage, $page.params.bookName, $books),
+				books.createPage(nextPage + 1, $page.params.bookName, $books)
+			]);
+
 			books.createPage(nextPage + 2, $page.params.bookName, $books);
+			books.createPage(nextPage + 3, $page.params.bookName, $books);
+
 			goto(`/book/${$page.params.bookName}/page/${nextPage}`);
 		}
 
 		if (event.key === 'ArrowLeft') {
-			const previousPage = currentIndex - 1 < 0 ? 0 : currentIndex - 1;
+			const previousPage =
+				currentIndex - numberOfPagesToIncrement < 0 ? 0 : currentIndex - numberOfPagesToIncrement;
 			goto(`/book/${$page.params.bookName}/page/${previousPage}`);
 		}
 	}
+
+	let showingTwoPages = false;
+	$: showingTwoPages = $isLandscapeMode && +$page.params.pageNumber !== 0;
 </script>
 
 <style>
 	.page-container {
 		display: grid;
 		place-items: center;
+	}
+
+	.landscape {
+		grid-template-columns: 1fr 1fr;
 	}
 
 	img {
@@ -42,4 +65,9 @@
 
 <svelte:window on:keyup={onArrow} />
 <div><a href="/book/{$page.params.bookName}">Pages</a></div>
-<div class="page-container"><img src={$pageUrl} alt="" /></div>
+<div class="page-container" class:landscape={showingTwoPages}>
+	<img src={$firstPageUrl} alt="" />
+	{#if showingTwoPages}
+		<img src={$secondPageUrl} alt="" />
+	{/if}
+</div>
